2e0f9c1769b1892f676b98ff53762c0410edc359
Pablo Montovani <pemontovani@gmail.com>
2022-02-03 21:32:55 -0300

    Modelos y administrador

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db.models import  Model, ImageField, ForeignKey, CASCADE
+from django.db.models.fields import (CharField, DateField)
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# Create your models here. 
+class Blog(Model):
+    titulo= CharField(max_length=50)
+    subtitulo= CharField(max_length=50)
+    autor= CharField(max_length=50)
+    fecha= DateField()
+    contenido= CharField(max_length=1000)
+    imagen= ImageField(upload_to='imagenes', null=True, blank=True)
+    
+class Avatar(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    imagen = ImageField(upload_to='avatares', null=True, blank=True)
+
